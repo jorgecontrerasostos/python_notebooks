2529a2a0a2dec466c0a89b17fdaa210a29f51b54
Jorge Contreras <86329049+jorgeostos@users.noreply.github.com>
2023-08-25 16:13:36 -0600

    add firt notebooks

---
+++ text_analyzer.py
@@ -0,0 +1,26 @@
+givenstring= "Lorem ipsum dolor! diam amet, consetetur Lorem magna. sed diam nonumy eirmod tempor. diam et labore? et diam magna. et diam amet."
+
+class TextAnalyzer(object):
+    def __init__(self, text):
+        formatted_text = text.replace('.', '').replace(',','').replace('!','').replace('?','')
+        formatted_text = formatted_text.lower()
+        self.formatted_text = formatted_text
+    
+    def freq_all(self):
+        word_list = self.formatted_text.split(' ')
+        freq_map = {}
+        for word in set(word_list):
+            freq_map[word] = word_list.count(word)
+        return freq_map
+    def freq_of(self, word):
+        freq_dict = self.freq_all()
+        if word in freq_dict:
+            return freq_dict[word]
+        else:
+            return 0
+        
+analyzed = TextAnalyzer(givenstring)
+freqMap = analyzed.freq_all()
+
+print('Formated Text:', analyzed.formatted_text)
+print(freqMap)
